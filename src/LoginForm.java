f5a56b6b728a0dc794086dc52cfdaf7d1177657b
JulioJuSalido <juliojusalido06@gmail.com>
2025-05-08 15:56:12 -0700

    Se implemento un if en el que aparece una ventana adicional al ingresar los datos

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -1,10 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginForm {
+public class LoginForm extends Component {
     private JPanel panelPrincipal;
     private JTextField usuarioTexto;
     private JPasswordField passwordTexto;
     private JButton enviarButton;
+
+    public LoginForm() {
+        enviarButton.addActionListener(e -> validar());
+    }
+
+    private void validar() {
+        // Leer todos los valore de los campos de texto
+        String usuario = this.usuarioTexto.getText();
+        String password = new String(this.passwordTexto.getPassword());
+
+        if(usuario.equals("admin") && password.equals("1234")){
+            //Datos correctos
+            JOptionPane.showMessageDialog(this, "Datos correctos, bienvenido!", "Bienvenido", JOptionPane.INFORMATION_MESSAGE);
+        } else if (usuario.equals("admin")) {
+            //usuario correcto y password incorrecto
+            JOptionPane.showMessageDialog(this, "Password incorrecto, corregirlo!", "Datos Incorrectos",JOptionPane.WARNING_MESSAGE);
+        } else {
+            //usuario incorrecto
+            JOptionPane.showMessageDialog(this, "Usuario incorrecto, corregirlo!", "Datos Incorrectos",JOptionPane.WARNING_MESSAGE);
+        }
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("LoginForm");
